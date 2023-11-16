81acce0c69bcbabf577c2feeaa5d4f0b0fe39178
Ayush <ayush.tesla@gmail.com>
2023-11-17 00:27:21 +0530

    Fixed routing

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var input = document.querySelectorAll("input");
-var btn = document.querySelector(".btn");
+var login_email = document.querySelector(".login-email");
 
-btn.addEventListener("click",() => {
-    if (input[0].value === "" || input[1].value === "") {
+var login_pass = document.querySelector(".login-pass");
+
+var log_btn = document.querySelector(".login-btn");
+
+
+log_btn.addEventListener("click",() => {
+    if (login_email.value === "" || login_pass.value === "") {
         alert("Fill the missing field");
     } else{
-        window.location.href = "reg.html";
+        document.querySelector(".congratulations").innerHTML = "<h1>CONGRATS YOU ARE LOGGED IN</h1><a href='/index.html'><button>Go Back</button></a>";
+        document.querySelector(".congratulations").classList.add("active");
+        document.querySelector(".login-box").classList.add("active");
     }
 });
+
---
+++ reg.js
@@ -0,0 +1,13 @@
+var reg_email = document.querySelector(".reg-email");
+var reg_pass = document.querySelector(".reg-pass");
+var reg_user = document.querySelector(".user");
+var reg_btn = document.querySelector(".register-btn");
+
+reg_btn.addEventListener("click",() => {
+    if (reg_email.value === "" || reg_pass.value === "" || reg_user.value === "") {
+        alert("Fill the missing field");
+    } else{
+        window.location.href = "/index.html";
+    }
+        
+});
